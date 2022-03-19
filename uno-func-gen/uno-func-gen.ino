8019c4e995e632edf1b654a42e9ec6a7accbd4bc
Diganta <diganta@Dev-Hub.domain.name>
2022-03-19 23:12:40 +0530

    changed the scanrate input mode to non-prompting and changed input format to accept submultiple values (to make it compatible with the Gui component of the system)
    clock pin and rate pin inversions to indicate change of input and change of direction (increasing or decreasing) respectively
    
    delay using _delay_loop_2 to get more hold loops for a given scanrate (so that for maximum required scan rate (in the current project) we get more than 1 hold loops allowing to easily implement clock pin inversion after hold_loops/2)
    
    scan rates as submultiples of maximum scan rate (maximum scan rate > maximum required scan rate so that hold loops > 1) so that we always get integral hold loops

--- uno-func-gen/uno-func-gen.ino
+++ uno-func-gen/uno-func-gen.ino
@@ -1,50 +1,99 @@
 #include <stdlib.h>
-
-float scanrate;     //scan rate in mV/s 
+ 
 int d_i=2000;   //Initial Delay 
-  
-const int steps =2000;//No. of Steps in 0-5V 
+
+const int32_t max_req_voltage = 1000; //Max Voltage required
 const int outPin = 5;  
- 
+const int outClock = A0;
+const int outRate = A1;
+
+const uint8_t FREQ_CPU = 16; //16MHz
+const uint16_t DEAD_ZONE = 58; //clock cycles required for pin state change. Experimentally measured, should change with FREQ_CPU.
+const uint8_t DELAY_LOOP_CYCLE = 4; //Microcontroller dependent. Should not change
+const uint16_t MULTIPLIER = 1000; //Should not change
+const uint16_t MAX_VOLTAGE = 5*MULTIPLIER; //Should not change
+const uint16_t steps =2*MULTIPLIER;//No. of Steps in 0-5V, should not change
+const uint16_t steps_del = 42; //Should not change, Experimentally measured
+const uint16_t max_steps = (max_req_voltage*steps)/MAX_VOLTAGE;
+
 void(* reset) (void) = 0;
- 
-void setup() { 
+
+int calc_submultiple() {
+  static_assert((FREQ_CPU%DELAY_LOOP_CYCLE)==0);
+  static_assert(max_steps > 2);
+  static_assert(steps_del<(steps-max_steps));
+  return ((MAX_VOLTAGE*(FREQ_CPU/DELAY_LOOP_CYCLE))*(1000000/steps))/steps;
+}
+
+int hold_loops;
+
+const int submultiple = calc_submultiple();
+void setup() {
+pinMode(outClock, OUTPUT);
+pinMode(outRate, OUTPUT);
+pinMode(outPin, OUTPUT);
+digitalWrite(outClock, HIGH);
+digitalWrite(outRate, LOW);
+
 Serial.begin(9600);
-Serial.setTimeout(60000);
-Serial.print("Enter scanrate (mv/s) [50 - 1250] : ");
-String value = Serial.readStringUntil('\r');
-scanrate = strtol(value.c_str(),0,10);
+Serial.setTimeout(INT32_MAX);
+Serial.print(submultiple);
+Serial.print(" ");
+Serial.print(4);
+Serial.print(" ");
+Serial.println(100);
+Serial.flush();
+
+String value = Serial.readStringUntil('\n');
+long scanrate = strtol(value.c_str(),0,10);
+
+hold_loops = round((submultiple*1.0f)/scanrate);
+float calc_scanrate = (submultiple*1.0f)/hold_loops;
+
 if (scanrate < 50 || scanrate  > 1250)
    {
-   Serial.println("Wrong scanrate");
+   Serial.println("error");
    Serial.flush();
    reset();
    }
-   Serial.println(scanrate);
+
+if(abs(calc_scanrate - scanrate)>1) {
+  Serial.println("error");
+  Serial.flush();
+  reset();
+}
+
+Serial.println(calc_scanrate);
+Serial.flush();
 } 
- 
-void loop() { 
-     float t=5000/((steps/1000)*(steps/1000)*scanrate); 
-     int T=(int)t;
-     int dT = (t-T)*(steps);
-     int cutVoltage = steps/5;
-    analogWrite(outPin, 1);                               
-    delay(d_i); 
-    int i = 0,m,ei;
-    while(true) { 
-       ei=(m%2)?cutVoltage-i:i;
-       pinMode(outPin, OUTPUT); 
-       for(int j=0;j<T;j++) { 
+
+__attribute__((always_inline)) inline void pwmGen(const int low, const int high, const int val) {
+       for(int j=low;j<high;j++) { 
           digitalWrite(outPin, HIGH); 
-          delayMicroseconds(ei); 
+          _delay_loop_2(val); 
           digitalWrite(outPin, LOW); 
-          delayMicroseconds(steps-ei); 
+          _delay_loop_2(steps-val-steps_del); 
+        }  
+}
+
+void loop() { 
+    delay(d_i); 
+    int i = 2,ei;
+    bool increasing = true;
+    digitalWrite(outRate, increasing);
+
+    noInterrupts();
+    while(true){
+       ei=(increasing)?i:max_steps-i;
+       digitalWrite(outClock, LOW);
+       pwmGen(0, hold_loops/2, ei);
+       digitalWrite(outClock, HIGH);
+       pwmGen(hold_loops/2, hold_loops, ei);
+       if(i==max_steps-1) {
+          i=1;
+          increasing=!increasing;
+          digitalWrite(outRate, increasing);
         }
-	if(dT) {
-	pinMode(outPin, INPUT);
-	delayMicroseconds(dT);
-	}
-	if(i==cutVoltage) {i=1;m++;}
-	i++;
-    } 
+        i++;               
+    }
  }
